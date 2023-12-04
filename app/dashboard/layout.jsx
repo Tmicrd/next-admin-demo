9b88f574b84844c9aeb134a7ff87b59988095452
nancy <liyingxiong23@gmail.com>
2023-12-05 00:12:37 +1100

    layout

---
+++ app/dashboard/layout.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Sidebar from "../ui/dashboard/sidebar/page";
+import Navbar from "../ui/dashboard/navbar/page";
+
+const Layout = ({ children }) => {
+  return (
+    <div>
+      <div>
+        <Sidebar />
+      </div>
+      <div>
+        <Navbar />
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
