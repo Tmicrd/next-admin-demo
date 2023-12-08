ea256f54930e6d7ff31a64666c02f6f11a508c6c
nancy <liyingxiong23@gmail.com>
2023-12-08 22:59:08 +1100

    reusable search component

--- app/ui/dashboard/navbar/navbar.jsx
+++ app/ui/dashboard/navbar/navbar.jsx
@@ -1,12 +1,8 @@
 "use client";
 import { usePathname } from "next/navigation";
-import {
-  MdNotifications,
-  MdOutlineChat,
-  MdPublic,
-  MdSearch,
-} from "react-icons/md";
+import { MdNotifications, MdOutlineChat, MdPublic } from "react-icons/md";
 import styles from "./navbar.module.css";
+import Search from "../search/search";
 
 const Navbar = () => {
   const pathname = usePathname();
@@ -14,9 +10,8 @@ const Navbar = () => {
     <div className={styles.container}>
       <div className={styles.title}>{pathname.split("/").pop()}</div>
       <div className={styles.menu}>
-        <div className={styles.search}>
-          <MdSearch />
-          <input type="text" placeholder="Search..." className={styles.input} />
+        <div>
+          <Search placeholder="Search..." />
         </div>
         <div className={styles.icons}>
           <MdOutlineChat size={20} />
